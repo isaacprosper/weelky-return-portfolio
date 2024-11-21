12cbf908e50f05c59e32913e3176130eb6798a7d
isaacprosper <isaacprosper39.com>
2024-11-21 12:34:40 +0100

    update

---
+++ css/color.php
@@ -0,0 +1,84 @@
+
+.counter__item:hover .icon, .footer-wrapper .footer-widget .links li a:hover, .menu li a.active, .section__cate, .faq__item.open .faq__title .title, .text--base, .user__item .user__content .amount, .client__item blockquote::after, .post__item .post__content .meta__date .meta__item i, .post__item .post__read, .footer-wrapper .footer-widget .links li a::before, .contact__item .contact__body a, .contact__item .contact__icon, a , .dashboard-item:hover .dashboard-icon, .widget__post .widget__post__content span, .page-link{
+    color: #FF6A00;
+}
+
+.form-control:focus{
+    border-color: #FF6A00fe;
+}
+
+.post__share li a i, *::selection, .header-top, .header-wrapper .right-area .cmn--btn, .cmn--btn, .feature-item .subtitle::before, .custom-tab-menu .tab-menu li.active .service-icon span, .section__cate::before, .section__cate::after, .faq__item.open .faq__title .right--icon::before, .footer-wrapper .footer-widget .title::after, .scrollToTop, button.cmn--btn:hover, .contact__item:hover .contact__icon, .account__section-wrapper .account__section-content .section__header .section__title::after, .dashboard-item .dashboard-icon, .dashboard-item:hover, .cmn--table thead th, .custom--card button.form--control, .widget__ticket-title::after{
+    background: #FF6A00;
+}
+
+.header-section.active .header-bottom, .footer-middle, .preloader{
+    background: #001933;
+}
+
+.feature-item .subtitle::before{
+    -webkit-animation: #FF6A00; infinite;
+}
+
+.custom-tab-menu::after{
+    border: 2px dashed #FF6A00;
+}
+
+.custom-tab-menu .tab-menu li .service-icon{
+    border: 1px solid #FF6A0066;
+}
+
+.how__thumb{
+    color: #FF6A00;
+    border-color: #FF6A00;
+}
+
+.post__item .post__content .meta__date{
+    border-left: 5px solid #FF6A00;
+}
+
+h1 a:hover, h2 a:hover, h3 a:hover, h4 a:hover, h5 a:hover, h6 a:hover , .feature-item .icon{
+    color: #FF6A00;
+}
+
+#preloader:before{
+    border: 1px solid #FF6A00;
+}
+
+@keyframes pulse {
+    0% {
+        box-shadow: 0 0 0 0 #FF6A0085, 0 0 0 0 #FF6A0085;
+    }
+
+    40% {
+        box-shadow: 0 0 0 20px #FF6A0000, #FF6A0085;
+    }
+
+    80% {
+        box-shadow: 0 0 0 20px #FF6A0000, 0 0 0 20px #FF6A0000;
+    }
+
+    100% {
+        box-shadow: 0 0 0 0 #FF6A0000, 0 0 0 0 #FF6A0000;
+    }
+}
+.user__item .user__thumb,
+.dashboard-menu ul li a:hover, .dashboard-menu ul li a.active{
+    border-color: #FF6A00;
+}
+
+.form--control-2:focus {
+    border-color: #FF6A00;
+}
+
+.btn--primary, .badge--primary, .bg--primary{
+    background: #FF6A00;
+}
+
+.page-item.active .page-link {
+    background-color: #FF6A00;
+    border-color: #FF6A00;
+}
+
+.page-link:hover, .see-more-less::after, .see-more-less.active span::after{
+    color: #FF6A00;
+}
